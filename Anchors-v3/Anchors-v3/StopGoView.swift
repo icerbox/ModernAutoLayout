7231c65db8d48a5afff7015c458a96aedfa53e16
IceR <icerbox@gmail.com>
2022-09-01 13:22:03 +0200

    Anchors-v3

--- Anchors-v3/Anchors-v3/StopGoView.swift
+++ Anchors-v3/Anchors-v3/StopGoView.swift
@@ -1,20 +1,71 @@
-//
-//  StopGoView.swift
-//  Anchors-v3
-//
-//  Created by Андрей Антонен on 31.08.2022.
-//
-
 import UIKit
 
-class StopGoView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+extension UIView {
+  var safeTopAnchor: NSLayoutYAxisAnchor {
+    if #available(iOS 11, *) {
+      return safeAreaLayoutGuide.topAnchor
+    } else {
+      return bottomAnchor
     }
-    */
+  }
+  var safeBottomAnchor: NSLayoutYAxisAnchor {
+    if #available(iOS 11, *) {
+      return safeAreaLayoutGuide.bottomAnchor
+    } else {
+      return topAnchor
+    }
+  }
+  var safeLeadingAnchor: NSLayoutXAxisAnchor {
+    if #available(iOS 11, *) {
+      return safeAreaLayoutGuide.leadingAnchor
+    } else {
+      return leadingAnchor
+    }
+  }
+  var safeTrailingAnchor: NSLayoutXAxisAnchor {
+    if #available(iOS 11, *) {
+      return safeAreaLayoutGuide.trailingAnchor
+    } else {
+      return trailingAnchor
+    }
+  }
+}
 
+final class StopGoView: UIView {
+  
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupView()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setupView()
+  }
+  
+  private let externalPadding: CGFloat = 50.0
+  private let internalSpacing: CGFloat = 25.0
+  
+  private let redView = UIView.makeView(color: .red)
+  private let greenView = UIView.makeView(color: .green)
+  
+  private func setupView() {
+    addSubview(redView)
+    addSubview(greenView)
+
+    NSLayoutConstraint.activate([
+      redView.leadingAnchor.constraint(equalTo: safeLeadingAnchor, constant: externalPadding),
+      greenView.leadingAnchor.constraint(equalTo: safeLeadingAnchor, constant: externalPadding),
+      
+      safeTrailingAnchor.constraint(equalTo: redView.trailingAnchor, constant: externalPadding),
+      safeTrailingAnchor.constraint(equalTo: greenView.trailingAnchor, constant: externalPadding),
+      
+      redView.topAnchor.constraint(equalTo: safeTopAnchor, constant: externalPadding),
+      greenView.topAnchor.constraint(equalTo: redView.bottomAnchor, constant: internalSpacing),
+      safeBottomAnchor.constraint(equalTo: greenView.bottomAnchor, constant: externalPadding),
+      
+      redView.heightAnchor.constraint(equalTo: greenView.heightAnchor)
+    ])
+  }
 }
--- Anchors-v3/Anchors-v3/ViewController.swift
+++ Anchors-v3/Anchors-v3/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  Anchors-v3
-//
-//  Created by Андрей Антонен on 31.08.2022.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+extension UIView {
+  static func makeView(color: UIColor) -> UIView {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.backgroundColor = color
+    return view
+  }
+}
+
+final class ViewController: UIViewController {
+  private let stopGoView: StopGoView = {
+    let view = StopGoView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.backgroundColor = .yellow
+    return view
+  }()
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setupView()
   }
-
+  
+  private func setupView() {
+    view.backgroundColor = .yellow
+    view.addSubview(stopGoView)
+    
+    NSLayoutConstraint.activate([
+      stopGoView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      stopGoView.topAnchor.constraint(equalTo: view.topAnchor),
+      stopGoView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      stopGoView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
+  }
 
 }
 
