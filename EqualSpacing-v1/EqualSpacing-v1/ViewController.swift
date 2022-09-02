80dbe19353f84b8f1edf997c48cc454470c0743a
IceR <icerbox@gmail.com>
2022-09-02 08:48:00 +0200

    Margins;2-v3

---
+++ EqualSpacing-v1/EqualSpacing-v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EqualSpacing-v1
+//
+//  Created by Андрей Антонен on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
